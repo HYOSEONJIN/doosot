b3e04ad464bb8e8a52498870aeeb1c3d17275d27
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2020-12-06 03:06:23 +0900

    1206 ver

---
+++ js/js.js
@@ -0,0 +1,15 @@
+function order_view() {
+
+/*    var editDiv = document.querySelector('div.edit_div');
+    editDiv.style.display = 'none';*/
+
+
+    var mainwrap =document.querySelector('div.main_wrap');
+    mainwrap.style.display = 'none';
+    
+    var order= document.querySelector('div.order_wrap'); 
+    order.style.display = 'block';
+    
+    return false;
+
+};
