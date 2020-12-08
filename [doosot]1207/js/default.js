3b2ccd694730cf8f322a2cf8b3e508335b662062
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-12-08 14:07:04 +0900

    클릭none

--- [doosot]1207/js/default.js
+++ [doosot]1207/js/default.js
@@ -1,20 +1,19 @@
-
 window.onload = function () {
     // 메뉴 불러오기
-    getItem();   
+    getItem();
 };
 
 
 /* 메뉴 불러오기 메서드 */
 function getItem() {
 
-    for(var i = 0; i < arrTabList.length; i++){
+    for (var i = 0; i < arrTabList.length; i++) {
 
         var menudata = JSON.parse(itemList[i]);
         var menuHtml = "";
-   
+
         for (var j = 0; j < menudata.length; j++) {
-            menuHtml += '<div class="menu" onclick = "javascript:addmenuOpen(' + i +', ' + j + ');">';
+            menuHtml += '<div class="menu" onclick = "javascript:addmenuOpen(' + i + ', ' + j + ');">';
             menuHtml += '<table>';
             menuHtml += '<tr>';
             menuHtml += '<td><img src="'
@@ -45,17 +44,17 @@ function getSidemenuItem(tabNum, itemNum) {
     $('#menu_name').text(menudata[itemNum].name);
     $("#view_img").attr("src", menudata[itemNum].src);
 
-    
+
     // 토핑 데이터 가져오기(음료수 탭만 제외 )
-   
-    if(tabNum != 3){    
-        var tabNum = 4; 
+
+    if (tabNum != 3) {
+        var tabNum = 4;
         var menudata = JSON.parse(itemList[tabNum]);
 
         var menuHtml = '<h2>밥 추가 변경</h2>';
 
         for (var i = 0; i < 6; i++) {
-            if(i == 3){
+            if (i == 3) {
                 menuHtml += '<h2>토핑 추가</h2>';
             }
             menuHtml += '<div class="addmenu" id="addmenu' + i + '" onclick="javascript:fnEtcClick(' + tabNum + ', ' + i + ');"><a1>';
@@ -66,51 +65,69 @@ function getSidemenuItem(tabNum, itemNum) {
         }
 
         console.log(menuHtml);
-       
+
 
         var div_side = document.querySelector("#div_addmenu");
         div_side.innerHTML = menuHtml;
-        $('#menuinfo').height("500px");  
-        
-    // 음료수 탭인 경우     
-    } else {        
+        $('#menuinfo').height("500px");
+
+        // 음료수 탭인 경우     
+    } else {
         var div_side = document.querySelector("#div_addmenu");
-        div_side.innerHTML = '';        
-        $('#menuinfo').height("300px");          
-    }   
+        div_side.innerHTML = '';
+        $('#menuinfo').height("300px");
+    }
 };
 
 
 // 메뉴탭 클릭시 메뉴목록 Display
-function menulist(tabNum){
-    for(var i = 0; i < arrTabList.length; i++){
-        if(i == tabNum){
+function menulist(tabNum) {
+    for (var i = 0; i < arrTabList.length; i++) {
+        if (i == tabNum) {
             eval("document.querySelector('#" + arrTabList[i] + "').style.display = 'block';");
-        }else {
+        } else {
             eval("document.querySelector('#" + arrTabList[i] + "').style.display = 'none';");
-        }                
+        }
     }
 };
 
-/*// 추가메뉴창 닫기 >> 제이쿼리로 변경
+// 추가메뉴창 닫기 >> 제이쿼리로 변경
 function addmenuClose() {
     document.querySelector('#menuinfo').style.display = 'none'
-    document.querySelector('#addmenu1').style.border= '0px';
-    
-};*/
+    document.querySelector('#addmenu1').style.border = '0px';
+    console.log('hey');
+
+    $('#bowl').removeClass('clicknone');
+    $('#square').removeClass('clicknone');
+    $('#side').removeClass('clicknone');
+    $('#beverage').removeClass('clicknone');
+    $('.menuselect >button').removeClass('clicknone');
+    $('.cart').removeClass('clicknone');
+
+};
 
 
 
 // [팝업창] 추가메뉴창 열기
 function addmenuOpen(tabNum, itemNum) {
-    
+
     // 팝업창 셋팅
-    getSidemenuItem(tabNum, itemNum);    
-    
+    getSidemenuItem(tabNum, itemNum);
+
     // 팝업창 노출 
     document.querySelector('#menuinfo').style.display = 'block';
-}
 
+    // 다른 곳 클릭 안되게하기
+    
+/*        $('#bowl').addClass('clicknone');
+        $('#square').addClass('clicknone');
+        $('#side').addClass('clicknone');
+        $('#beverage').addClass('clicknone');
+        $('.menuselect >button').addClass('clicknone');
+        $('.cart').addClass('clicknone');
+    */
+
+}
 
 /*매장or포장*/
 function takeout(takeout) {
@@ -144,29 +161,27 @@ function removeCart() {
         removecart.innerHTML = removeHtml;
         var removecart = document.getElementById("sum");
         removecart.innerHTML = removeHtml;
-        
+
     }
 };
 
 
 // 토핑 클릭 
 var toppingList = [];
-function fnEtcClick(tabNum, itemNum){
+
+function fnEtcClick(tabNum, itemNum) {
 
     // 토핑인 경우 
-    if(toppingList[itemNum] > 0){    
+    if (toppingList[itemNum] > 0) {
         // 이미 선택되어져 있는경우 선택 해제 
         $('#addmenu' + itemNum).css('border', '0px solid red');
         toppingList[itemNum] = 0;
 
-    }else {
+    } else {
         // 선택 안되어 있는 경우 선택
         $('#addmenu' + itemNum).css('border', '3px solid red');
         toppingList[itemNum] = 1;
     }
 
- 
+
 }
-
-
-
